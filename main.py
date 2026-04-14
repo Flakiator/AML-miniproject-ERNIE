d3c90e1f6e2d97eba38abc4672ed3248a06d2659
AntohaY <antonyakovenko2401@gmail.com>
2026-04-14 21:24:47 +0200

    added initial setup for weights and biases

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from datasets import load_dataset
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 from torch.utils.data import DataLoader
 import torch
+import wandb
+
+def setup_wandb():
+    wandb.login()
+    return wandb.init(
+        entity="ERNIE-AML-2026",
+        project="aml-miniproject-ernie",
+        # ...
+    )
+
+wandb_run = setup_wandb()
 # Data Retrieval
 data = load_dataset("stanfordnlp/imdb")
 train_data, test_data = data["train"], data["test"]
+
 # Create a validation set from the training data
 split_idx = int(0.9 * len(train_data))
 val_data = train_data.select(range(split_idx, len(train_data)))
